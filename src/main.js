9be9c622770babc8427008c08b186ea70195cc39
m-ughetto <mathias.ughetto@gmail.com>
2020-04-10 22:35:28 +0200

    Ajout de frameworks

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import jquery from 'jquery'
+import 'bootstrap/dist/js/bootstrap.min'
 import Vue from 'vue'
 import App from './App'
+
+window.$ = jquery
+window.jQuery = jquery
 
 Vue.config.productionTip = false
 
