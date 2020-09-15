25e4093c4a0fafc2c1bd050353d9d0a24bf81eca
Mathias Ughetto <mathias.ughetto@gmail.com>
2020-09-15 10:31:01 +0200

    Fix version

--- src/App.vue
+++ src/App.vue
@@ -376,7 +376,7 @@
             </p>
             <hr>
             <p>
-              EDT EPSI - Version: 1.0.4
+              EDT EPSI - Version: 1.0.5
               <br>
               Pour toute demande, envoyer un mail à <a href="mailto:contact@epsi.best?subject=[EDT EPSI]">contact@epsi.best</a>
             </p>
