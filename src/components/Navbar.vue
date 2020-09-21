b2d262c34fa7b8cb267eb81e51fbc9dbf1458a1a
MAKSIM-BRY <maxim.barry@gmail.com>
2020-09-21 23:45:34 +0200

    Mise en place composant Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="navbar">
+    <nav class="navbar navbar-dark bg-epsi-dark fixed-top">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#" @click.prevent='resetDate'><i class="fas fa-sync"></i></a>
+        </li>
+      </ul>
+      <span class="navbar-text mr-auto text-white text-capitalize">{{ showMonthYear }}</span>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#" data-toggle="modal" data-target="#configModal" @click="loadConfig"><i class="fas fa-cog"></i></a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    showMonthYear: String,
+    resetDate: Function,
+    loadConfig: Function
+  }
+}
+</script>
+
+<style>
+
+</style>
